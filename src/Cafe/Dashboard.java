16612021fedb533f63493e09ab0c773df13f7a36
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-03-08 09:38:20 +0530

    shit didnt work fck

--- src/Cafe/Dashboard.java
+++ src/Cafe/Dashboard.java
@@ -1,4 +1,12 @@
 package Cafe;
 
+import javax.swing.*;
+
 public class Dashboard {
+    private JPanel panel1;
+    private JButton button1;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
