602e9875e267aa38cb1caa9493b44e603c89e684
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-06-07 23:38:57 +0530

    added new package removed previous one

--- Eleczone/src/java/img/addimage.java
+++ Eleczone/src/java/img/addimage.java
@@ -13,6 +13,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
 
 /**
@@ -34,6 +35,7 @@ public class addimage extends HttpServlet {
             throws ServletException, IOException {
       
         System.out.println("test");
+        Part file = request.getPart("image");
         
     }
 
--- Eleczone/web/index.jsp
+++ Eleczone/web/index.jsp
@@ -17,7 +17,7 @@
         <div align="center">
             <form action="addimage" method="post" enctype="multipart/form-data">
             select image :
-            <input type="file"> 
+            <input type="file" name="image"> 
             <input type="submit" value="Add image">
         </form>
         </div>
