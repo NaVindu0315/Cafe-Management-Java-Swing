8fcaa481ac7fa33baf93adcbaa64f770afc120d0
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-06-08 12:52:05 +0530

    setting cookies done

--- Eleczone/src/java/cookiepackage/getcookies.java
+++ Eleczone/src/java/cookiepackage/getcookies.java
@@ -8,6 +8,8 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Cookie;
+import javax.servlet.RequestDispatcher;
 
 /**
  *
@@ -38,8 +40,20 @@ public class getcookies extends HttpServlet {
         response.setContentType("text/html");
         String uname = request.getParameter("username");
         String itmname = request.getParameter("itemname");
-        System.out.println(uname);
-        System.out.println(itmname);
+        //System.out.println(uname);
+        //System.out.println(itmname);.
+        //to set values to cookie
+        Cookie cref1 = new Cookie("cuname",uname);
+        Cookie cref2 = new Cookie("citemname",itmname);
+        //to set the age of the cookie
+        cref1.setMaxAge(365*24*60*60);
+        cref2.setMaxAge(365*24*60*60);
+        //adding cookies
+        response.addCookie(cref1);
+        response.addCookie(cref2);
+        
+        RequestDispatcher rq = request.getRequestDispatcher("displaycookies");
+        rq.forward(request, response);
         
         
         
