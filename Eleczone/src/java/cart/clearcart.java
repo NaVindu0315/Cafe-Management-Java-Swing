495b927e552961ccaf1437c9f4e175320436997a
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-06-09 22:57:19 +0530

    cookie removing works

--- Eleczone/src/java/cart/clearcart.java
+++ Eleczone/src/java/cart/clearcart.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -33,6 +34,19 @@ public class clearcart extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        Cookie ckitemname1 = new Cookie("ckitemname1", "");
+        ckitemname1.setMaxAge(0);
+        response.addCookie(ckitemname1);
+        
+        Cookie ckitem1 = new Cookie("ckitem1", "");
+        ckitem1.setMaxAge(0);
+        response.addCookie(ckitem1);
+        
+        Cookie ckitemtotal1 = new Cookie("ckitemtotal1", "");
+        ckitemtotal1.setMaxAge(0);
+        response.addCookie(ckitemtotal1);
+        
+        response.sendRedirect("listings.jsp");
         
     }
 
