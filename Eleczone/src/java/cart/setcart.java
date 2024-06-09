375cd27dec400a9dfbba40c1a2015a59f42b317c
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-06-09 12:55:00 +0530

    set cart  functions to add to cookies

--- Eleczone/src/java/cart/setcart.java
+++ Eleczone/src/java/cart/setcart.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -53,15 +54,68 @@ public class setcart extends HttpServlet {
          Integer item3total = 315000 * item3;
          
          Integer Total = item1total + item2total+ item3total;
+         ///for item names
+         Cookie ckitemname1 = new Cookie("ckitemname1",item1name);
+         Cookie ckitemname2 = new Cookie("ckitemname2",item2name);
+         Cookie ckitemname3 = new Cookie("ckitemname3",item3name);
+         
+         ///item quantity
+         Cookie ckitem1 = new Cookie("ckitem1", String.valueOf(item1));
+         Cookie ckitem2 = new Cookie("ckitem2", String.valueOf(item2));
+         Cookie ckitem3 = new Cookie("ckitem3", String.valueOf(item3));
+         ///item total
+         Cookie ckitemtotal1 = new Cookie("ckitemtotal1",String.valueOf(item1total));
+         Cookie ckitemtotal2 = new Cookie("ckitemtotal2",String.valueOf(item2total));
+         Cookie ckitemtotal3 = new Cookie("ckitemtotal3",String.valueOf(item3total));
+         //total
+         Cookie cktotal = new Cookie("cktotal",String.valueOf(Total));
+         ///to set the age 
+         ckitemname1.setMaxAge(365*24*60*60);
+         ckitemname2.setMaxAge(365*24*60*60);
+         ckitemname3.setMaxAge(365*24*60*60);
+         ckitem1.setMaxAge(365*24*60*60);
+         ckitem2.setMaxAge(365*24*60*60);
+         ckitem3.setMaxAge(365*24*60*60);
+         ckitemtotal1.setMaxAge(365*24*60*60);
+         ckitemtotal2.setMaxAge(365*24*60*60);
+         ckitemtotal3.setMaxAge(365*24*60*60);
+         cktotal.setMaxAge(365*24*60*60);
+         ///to set cookies
+         //1
+         response.addCookie(ckitemname1);
+         //2
+         response.addCookie(ckitemname2);
+         //3
+         response.addCookie(ckitemname3);
+         //4
+         response.addCookie(ckitem1);
+         //5
+          response.addCookie(ckitem2);
+         //6
+          response.addCookie(ckitem3);
+         //7
+          response.addCookie(ckitemtotal1);
+         //8
+         response.addCookie(ckitemtotal2);
+         //9
+         response.addCookie(ckitemtotal3);
+         //10
+         response.addCookie(cktotal);
+         
+         //navigate to display cart
+         response.sendRedirect("displaycookies");
+         
+         
+         
          
          
          
          ///to check values 
-         System.out.println("Item name           Qty            Total");
+         /*System.out.println("Item name           Qty            Total");
          System.out.println(item1name+"         "+item1 +"     "+item1total);
          System.out.println(item2name+"         "+item2 +"     "+item2total);
          System.out.println(item3name+"         "+item3 +"     "+item3total);
-         System.out.println("                                  "+Total);
+         System.out.println("                                  "+Total);*/
          /*
          System.out.println("number");
          System.out.println(item1);
@@ -72,6 +126,18 @@ public class setcart extends HttpServlet {
          System.out.println(item2name);
          System.out.println(item3name);
          */
+                 /*
+                  ckitemname1
+         ckitemname2
+         ckitemname3
+         ckitem1
+         ckitem2
+         ckitem3
+         ckitemtotal1
+         ckitemtotal2
+         ckitemtotal3
+         cktotal
+                 */
         
         
     }
