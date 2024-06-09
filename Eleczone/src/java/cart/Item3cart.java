730ab1ec839c5b5e2eaf4d20b4855ea37cd0cd97
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-06-09 22:46:26 +0530

    Data stores

--- Eleczone/src/java/cart/Item3cart.java
+++ Eleczone/src/java/cart/Item3cart.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -33,7 +34,24 @@ public class Item3cart extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+       
+        response.setContentType("text/html");
+        String item3name = request.getParameter("itemname3");
+        Integer item3 = Integer.parseInt(request.getParameter("itemcount3"));
+        Integer item3total = 315000 * item3;
+        
+        Cookie ckitemname3 = new Cookie("ckitemname3",item3name);
+        Cookie ckitem3 = new Cookie("ckitem3", String.valueOf(item3));
+        Cookie ckitemtotal3 = new Cookie("ckitemtotal3",String.valueOf(item3total));
+        
+        ckitemname3.setMaxAge(365*24*60*60);
+        ckitem3.setMaxAge(365*24*60*60);
+        ckitemtotal3.setMaxAge(365*24*60*60);
+        response.addCookie(ckitemname3);
+        response.addCookie(ckitem3);
+        response.addCookie(ckitemtotal3);
+        response.sendRedirect("displaycart");
+        
     }
 
  
